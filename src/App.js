35e40963d9a9d80613623347fc7920ec542bfd6e
Manish Veerani <manishveerani0208@gmail.com>
2022-09-05 20:39:10 +0530

    setup MovieList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+// branch -> movie-ott
 
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
 
 function App() {
+
+  const [movies, setMovies] = useState([
+    {
+      "Title": "The Avengers",
+      "Year": "2012",
+      "imdbID": "tt0848228",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+    },
+    {
+      "Title": "Avengers: Endgame",
+      "Year": "2019",
+      "imdbID": "tt4154796",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+    },
+    {
+      "Title": "Avengers: Infinity War",
+      "Year": "2018",
+      "imdbID": "tt4154756",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    },
+    {
+      "Title": "Avengers: Age of Ultron",
+      "Year": "2015",
+      "imdbID": "tt2395427",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg"
+    }
+  ]);
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="container-fluid movie-app">
+      <div className='row'>
+        <MovieList
+          movies = {movies}
+        />
+      </div>
+      
     </div>
   );
 }
