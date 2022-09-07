e1cae32abd004122d7271bd6c7fc0a668cb2b361
Manish Veerani <manishveerani0208@gmail.com>
2022-09-07 22:35:25 +0530

    setup MovieListHeading & SearchBox

--- src/App.js
+++ src/App.js
@@ -4,42 +4,20 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 function App() {
 
-  const [movies, setMovies] = useState([
-    {
-      "Title": "The Avengers",
-      "Year": "2012",
-      "imdbID": "tt0848228",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Avengers: Endgame",
-      "Year": "2019",
-      "imdbID": "tt4154796",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Avengers: Infinity War",
-      "Year": "2018",
-      "imdbID": "tt4154756",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Avengers: Age of Ultron",
-      "Year": "2015",
-      "imdbID": "tt2395427",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
   return (
     <div className="container-fluid movie-app">
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className='row'>
         <MovieList
           movies = {movies}
---
+++ src/components/MovieListHeading.jsx
@@ -0,0 +1,11 @@
+
+const MovieListHeading = (porps) => {
+    return(
+        <div className="col">
+            <h1>{porps.heading}</h1>
+        </div>
+    )
+}
+
+
+export default MovieListHeading;
